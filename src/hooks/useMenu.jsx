2a82046779cf092a2c45f95d29f12f21bacf2007
yeasinmiah1272001 <137316520+yeasinmiah1272001@users.noreply.github.com>
2025-01-15 22:39:10 +0600

    complete

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const useMenu = () => {
   const [menu, setMenu] = useState([]);
   const [loading, setLoading] = useState(true);
-  // console.log("menu", menu);
+
   useEffect(() => {
     fetch("http://localhost:8000/menu")
       .then((res) => res.json())
